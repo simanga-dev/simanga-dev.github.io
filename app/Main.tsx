6418242d80a1f84c596e1233c0c036ae01f7360a
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-01-06 13:56:35 +0200

    build(fix-deploy): update code to fix the build that is failing

--- app/Main.tsx
+++ app/Main.tsx
@@ -9,7 +9,7 @@ const MAX_DISPLAY = 5
 export default function Home({ posts }) {
   return (
     <>
-      <div className="divide-y divide-gray-200 pt-48 dark:divide-gray-700 ">
+      <div className="divide-y divide-gray-200 pt-16 dark:divide-gray-700 ">
         {/* <div className="space-y-2 pb-8 pt-6 md:space-y-5"> */}
         {/*   <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"> */}
         {/*     Latest */}
@@ -23,7 +23,7 @@ export default function Home({ posts }) {
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags } = post
             return (
-              <li key={slug} className="py-12">
+              <li key={slug} className="py-6">
                 <article>
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
                     <dl>
